33eb18e09fc3ffd90ba57a09a670b137951838d6
ThuanyMendonca <thuany1997@hotmail.com>
2018-05-17 21:28:53 -0300

    Script

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ var body = require('body-parser');
 
 app = express();
 
+app.use(body.urlencoded({extended:true}));
+app.use(body.json());
+
 app.set('port', 3000);
 
 var mysql = require('mysql');
@@ -42,12 +45,14 @@ var client = mysql.createConnection({
 client.query('USE ' + database);
 
 // inserir um registro na tabela
-/*client.query(
-    'INSERT INTO ' + tabela + '(cor_codigo, cor_descricao, cor_referencia) values (NULL, "AZUL", "005")'
+/*
+client.query(
+    'INSERT INTO ' + tabela + '(cor_codigo, cor_descricao, cor_referencia) values (NULL, "VERMELHA", "006")'
 );
 */
 
 
+// mostrar o resultado da pesquisa (JSON) no navegador
 app.get('/cores', function(req, res)
 {
     client.query
@@ -60,14 +65,56 @@ app.get('/cores', function(req, res)
             throw err;
         }
 // enviar o resultado (retorno query) para o tamplete
-        //res.send(results);
-        res.render('../views/lista_cores.ejs',{cores:results, title:'Lista'});
+//        res.send(results);
+        res.render('../views/lista_cores.ejs', {cores: results, title:'Lista'  })
     }
 
     )
 }
 )
 
+
+// mostrar o resultado da pesquisa (JSON) no navegador
+app.get('/cores/views/:codigo', function(req, res)
+{
+console.log(req.params.codigo)
+    client.query
+    (
+    'SELECT * FROM ' + tabela + ' WHERE cor_codigo = ' + parseInt(req.params.codigo), 
+
+    function(err, result, fields)
+    {
+        if(err)
+        {
+            throw err;
+        }
+// enviar o resultado (retorno query) para o tamplete
+        res.render('../views/lista_cores_unica.ejs', {cor: result, title:result[0].cor_descricao})
+    }
+
+    )
+}
+)
+
+
+// adicionar novo registro na tabela 
+app.get('/cores/add', function(req, res)
+{
+    res.render('form_cores', {title:'Cadastro de Cores'});
+})
+
+app.post('/cores/add', function(req, res)
+{
+    var dados = req.body;
+    console.log("Descrição..:" + req.body.cor_descricao);
+    console.log("Referencia.:" + req.body.cor_referencia);
+
+    var msql = 'INSERT INTO ' + tabela + ' SET ? ';
+    client.query( msql, dados);
+
+    res.redirect('/cores');    
+}
+)
 
 
 http.createServer(app).listen(app.get('port'), function() {
